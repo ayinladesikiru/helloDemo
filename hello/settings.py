2f0d06a8733b3dedfce9a1b113e025140448bb2d
ayinlade sikiru <ayinladesikiru@gmail.com>
2025-02-02 19:42:45 +0100

    whitenoise and gunicorn

--- hello/settings.py
+++ hello/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = "hello.urls"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 TEMPLATES = [
